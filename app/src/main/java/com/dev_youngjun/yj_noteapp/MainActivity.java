076894c12071b7b06c6060e4ff3c9a8160897731
jun108059 <youngjun108059@gmail.com>
2020-07-02 22:42:00 +0900

    [Add] ListView With ArrayList

--- app/src/main/java/com/dev_youngjun/yj_noteapp/MainActivity.java
+++ app/src/main/java/com/dev_youngjun/yj_noteapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.dev_youngjun.yj_noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        notes.add("Example note");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
+        listView.setAdapter(arrayAdapter);
+
     }
+
 }
