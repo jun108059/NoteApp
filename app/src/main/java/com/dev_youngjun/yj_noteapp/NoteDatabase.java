bb95091ee450e79cb3320e1d83ddc7a1192e5dab
jun108059 <youngjun108059@gmail.com>
2020-07-02 22:01:08 +0900

    [Update] SQLite Query & execSQL

--- app/src/main/java/com/dev_youngjun/yj_noteapp/NoteDatabase.java
+++ app/src/main/java/com/dev_youngjun/yj_noteapp/NoteDatabase.java
@@ -10,12 +10,29 @@ public class NoteDatabase extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "notedb";
     private static final String DATABASE_TABLE = "notestable";
 
+    // columns name for database table
+    private static final String KEY_ID = "id";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_CONTENT = "content";
+    private static final String KEY_DATE = "date";
+    private static final String KEY_TIME = "time";
+
+
     NoteDatabase(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // CREATE TABLE nametame(id INT PRIMARY KEY, title TEXT, content text, data TEXT, time TEXT);
+        // 데이터베이스 테이블 생성
+        String query = "CREATE TABLE " + DATABASE_TABLE + "(" + KEY_ID + " INT PRIMARY KEY," +
+                KEY_TITLE + " TEXT," +
+                KEY_CONTENT + " TEXT," +
+                KEY_DATE + " TEXT," +
+                KEY_TIME + " TEXT," + ")";
+        db.execSQL(query);
+        // 쿼리를 통한 DB 생성하는 코드
 
     }
 
