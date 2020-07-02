d2caf838ffa2aecedc5a6849f24a25344eac362f
jun108059 <youngjun108059@gmail.com>
2020-07-02 21:54:59 +0900

    [Add] Database Class - SQLite

---
+++ app/src/main/java/com/dev_youngjun/yj_noteapp/NoteDatabase.java
@@ -0,0 +1,27 @@
+package com.dev_youngjun.yj_noteapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class NoteDatabase extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "notedb";
+    private static final String DATABASE_TABLE = "notestable";
+
+    NoteDatabase(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
